95114a8ce9056e16e8f5b711176240cca59835b2
darkfusion-9000 <81690223+darkfusion-9000@users.noreply.github.com>
2022-03-20 19:51:06 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload() {
+    
+}
+function setup() {
+canvas = createCanvas(500,500);
+canvas.center();
+}
+function draw() {
+
+}
+function take_snapshot() {
+save(' bhg bh cv kngf vbnvbbvf v /n vc,.png ')
+}
